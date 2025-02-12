1c75aa12f3359bb7b05cb72154a5c6cf0d24259d
Alexander Zeman <22159156+alxjpzmn@users.noreply.github.com>
2025-02-12 21:52:55 +0100

    Fix menu bar color scheme, fix tracing

--- core/src/api/mod.rs
+++ core/src/api/mod.rs
@@ -8,9 +8,6 @@ pub mod routes;
 
 pub async fn api() -> anyhow::Result<()> {
     let router = create_router()?;
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
-        .init();
     let addr = SocketAddr::from(([0, 0, 0, 0], 8084));
     let listener = TcpListener::bind(&addr).await?;
     Ok(axum::serve(listener, router.into_make_service()).await?)
