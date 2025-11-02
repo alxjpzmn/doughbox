b82044dda1c4edafc9976897def5cd853afd78e6
alxjpzmn <22159156+alxjpzmn@users.noreply.github.com>
2025-11-02 10:00:31 +0100

    Fix incorrect migration SQL

--- core/migrations/V3__fix_withholding_tax_currency.sql
+++ core/migrations/V3__fix_withholding_tax_currency.sql
@@ -8,16 +8,16 @@ BEGIN
     IF EXISTS (SELECT 1 FROM pg_catalog.pg_attribute 
                WHERE attname = 'witholding_tax_currency' 
                AND attrelid = 'interest'::regclass) THEN
-        EXECUTE 'ALTER TABLE trade RENAME COLUMN witholding_tax_currency TO withholding_tax_currency';
+        EXECUTE 'ALTER TABLE interest RENAME COLUMN witholding_tax_currency TO withholding_tax_currency';
     END IF;
     IF EXISTS (SELECT 1 FROM pg_catalog.pg_attribute 
                WHERE attname = 'witholding_tax_currency' 
                AND attrelid = 'dividend'::regclass) THEN
-        EXECUTE 'ALTER TABLE trade RENAME COLUMN witholding_tax_currency TO withholding_tax_currency';
+        EXECUTE 'ALTER TABLE dividend RENAME COLUMN witholding_tax_currency TO withholding_tax_currency';
     END IF;
     IF EXISTS (SELECT 1 FROM pg_catalog.pg_attribute 
                WHERE attname = 'witholding_tax_currency' 
                AND attrelid = 'fx_conversion'::regclass) THEN
-        EXECUTE 'ALTER TABLE trade RENAME COLUMN witholding_tax_currency TO withholding_tax_currency';
+        EXECUTE 'ALTER TABLE fx_conversion RENAME COLUMN witholding_tax_currency TO withholding_tax_currency';
     END IF;
 END $$;
